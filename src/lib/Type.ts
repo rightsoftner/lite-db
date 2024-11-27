87c2c4991a2f3bca3a58c2be3d24767fd96c5027
Volodymyr Nerovnia <nerv@i.ua>
2024-11-27 08:04:12 +0200

    Updated ResponseUserType

--- src/lib/Type.ts
+++ src/lib/Type.ts
@@ -20,7 +20,22 @@ export type UserDetailType = Static<typeof UserDetail>;
 
 // RequestBody
 export type RequestBodyType = Static<typeof User> & Static<typeof UserDetail>;
-export type ResponseUserType = Static<typeof User> & Static<typeof UserDetail>;
+
+export type ResponseUserType = {
+  user_id: string;
+  name: string;
+  email: string;
+  password: string;
+  u_created_at: Date;
+  u_updated_at: Date;
+  udetail_id: string;
+  nick: string;
+  fullName: string;
+  ud_created_at: Date;
+  ud_updated_at: Date;
+};
+
+//export type ResponseUserType = Static<typeof User> & >;
 
 // Profile
 export const Profile = Type.Object({
